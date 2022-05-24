5da982fd269f0147f8c0be6a8310f1653e5b75d8
Esteban Monguí <esteband.itorres@gmail.com>
2022-05-24 18:02:37 -0500

    feat: create header base

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/css/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header__wrapper'>
+      <h1 className='header__logo'>Sologram</h1>
+    </div>
+  )
+}
+
+export default Header
