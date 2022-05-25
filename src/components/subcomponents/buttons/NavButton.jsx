4e8baa741ead036976cd76e038298c27b878c251
Esteban Monguí <esteband.itorres@gmail.com>
2022-05-24 20:07:33 -0500

    feat: add logic to validate kind of button to render and its style

--- src/components/subcomponents/buttons/NavButton.jsx
+++ src/components/subcomponents/buttons/NavButton.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../../../assets/css/NavButton.css'
 
 const NavButton = ({ iconClass, text, path }) => {
+
   return (
     <button className='nav-button__wrapper'>
-      <Link to={ path }>
-        <i className={ iconClass }>{ text }</i>
-      </Link>
+      <NavLink
+        activeClassName="active"
+        to={ path }
+      >
+        {
+          !!text
+          ?
+          <div className={ `icon nav-button__text` }>{ text }</div>
+          :
+          <i className={ `${iconClass} icon` }></i>
+        }
+      </NavLink>
     </button>
   )
 }
