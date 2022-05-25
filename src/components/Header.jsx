fbf722e9b634d92f72d0cdd6c8ad10f35c482f36
Esteban Monguí <esteband.itorres@gmail.com>
2022-05-24 20:05:50 -0500

    feat: add link to header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../assets/css/Header.css'
 
 const Header = () => {
   return (
     <div className='header__wrapper'>
-      <h1 className='header__logo'>Sologram</h1>
+      <Link to='/'>
+        <h1 className='header__logo'>Sologram</h1>
+      </Link>
     </div>
   )
 }
